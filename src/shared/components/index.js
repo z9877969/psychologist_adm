ac5bb5129d0d6b384e5fd1cdf5e27cd75f192a78
z9877969 <z9877969@ukr.net>
2024-05-03 14:14:54 +0300

    deletes no-used code

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -1,6 +1,4 @@
 export { default as SharedLayout } from './SharedLayout/SharedLayout';
-export { default as Logo } from './Logo/Logo';
-export { default as Container } from './Container/Container';
 export { default as Loader } from './Loader/Loader';
 export { default as Toastify } from './Toastify/Toastify';
 export { default as FieldsGroupWrapper } from './FieldsGroupWrapper';
