cd30eeeaf1f083e140057452d5a867081a154f0b
z9877969 <z9877969@ukr.net>
2024-05-06 18:32:10 +0300

    fixes baseUrl

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const baseInstance = axios.create({
   // baseURL: 'http://localhost:4040/api',
-  baseURL: 'https://api.brushbuddy.com.ua/api',
+  baseURL: 'https://api.irynaprudko.com.ua/api',
 });
